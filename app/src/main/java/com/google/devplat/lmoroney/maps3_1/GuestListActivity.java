0c5aa5aee5d375711932b60a3d8de820840955e8
divyanshgupta97 <divyanshguptarocking@gmail.com>
2016-01-24 03:34:55 +0800

    blah

--- app/src/main/java/com/google/devplat/lmoroney/maps3_1/GuestListActivity.java
+++ app/src/main/java/com/google/devplat/lmoroney/maps3_1/GuestListActivity.java
@@ -9,7 +9,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,9 +19,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GuestListActivity extends ActionBarActivity {
+
     private ArrayAdapter<String> mContactsAdapter;
-    public  ListView listView;
-    private final String LOG_TAG = GuestListActivity.class.getSimpleName();
+    public LinearLayout linearList ;
+    CheckBox checkBox;
+    ArrayList<String> finalArray = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +33,39 @@ public class GuestListActivity extends ActionBarActivity {
 
         String[] ContactsArray = {"Pratyum", "Shantanu", "Priyanshu", "Divyansh", "Varun", "Manav"};
 
-        List<String> Logs = new ArrayList<>(Arrays.asList(ContactsArray));
+        ArrayList<String> Logs = new ArrayList<>(Arrays.asList(ContactsArray));
 
        // ArrayList<String> Logs = getIntent().getSerializableExtra("KEY");
 
-        mContactsAdapter = new ArrayAdapter<>(this, R.layout.checkboxlist, R.id.friend_checkbox, Logs);
+//        mContactsAdapter = new ArrayAdapter<>(this, R.layout.checkboxlist, R.id.friend_checkbox, Logs);
+//
+      linearList = (LinearLayout) findViewById(R.id.linear_list);
+//        //listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+//
+//        listView.setAdapter(mContactsAdapter);
 
-        listView = (ListView) findViewById(R.id.guest_listview);
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-
-        listView.setAdapter(mContactsAdapter);
+        for(int i=0; i<Logs.size(); i++)
+        {
+            checkBox=new CheckBox(this);
+            checkBox.setId(i);
+            checkBox.setText(Logs.get(i));
+            checkBox.setOnClickListener(getOnClickDo(checkBox));
+            linearList.addView(checkBox);
+        }
     }
-
+    View.OnClickListener getOnClickDo(final Button b)
+    {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(finalArray.contains(b.getText().toString()))
+                    finalArray.remove(b.getText().toString());
+                else
+                    finalArray.add(b.getText().toString());
+            }
+        };
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -64,27 +90,6 @@ public class GuestListActivity extends ActionBarActivity {
 
     public void confirm(View view)
     {
-        SparseBooleanArray checked = listView.getCheckedItemPositions();
-        Log.d(LOG_TAG,checked.toString());
-        ArrayList<String> finalArray = new ArrayList<>();
-//        if (checked != null) {
-//            for (int i=0; i<checked.size(); i++) {
-//                if (checked.valueAt(i)) {
-//                    String item = listView.getAdapter().getItem(
-//                            checked.keyAt(i)).toString();
-//                    Log.d(LOG_TAG, item + " was selected");
-//                    finalArray.add(mContactsAdapter.getItem(i));
-//                }
-//            }
-//        }
-        if(checked!=null) {
-            for (int i = 0; i < mContactsAdapter.getCount(); i++) {
-                if (checked.get(i)) {
-                    finalArray.add(mContactsAdapter.getItem(i));
-                    Log.d(LOG_TAG, mContactsAdapter.getItem(i));
-                }
-            }
-        }
         Intent intent = new Intent(this,Page2Activity.class);
         intent.putExtra("KEY",finalArray);
         startActivity(intent);
