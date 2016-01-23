b5f2f23494f12bd1109cd3ff3f1525b5ce906c35
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-01-23 20:08:19 +0800

    fdghj

--- app/src/main/java/com/google/devplat/lmoroney/maps3_1/Page2Activity.java
+++ app/src/main/java/com/google/devplat/lmoroney/maps3_1/Page2Activity.java
@@ -1,8 +1,8 @@
 package com.google.devplat.lmoroney.maps3_1;
 
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,7 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import java.io.BufferedReader;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,19 +22,24 @@ import java.util.List;
 
 public class Page2Activity extends ActionBarActivity {
     private ArrayAdapter<String> mLogsAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
-        String[] LogsArray={"Pratyum","Shantanu","Priyanshu","Divyansh","Varun","Manav"};
+        String[] LogsArray = {"Pratyum", "Shantanu", "Priyanshu", "Divyansh", "Varun", "Manav"};
 
         List<String> Logs = new ArrayList<>(Arrays.asList(LogsArray));
 
 
-        mLogsAdapter= new ArrayAdapter<>(this,R.layout.list_logs,R.id.list_logs_textview,Logs);
+        mLogsAdapter = new ArrayAdapter<>(this, R.layout.list_logs, R.id.list_logs_textview, Logs);
 
-        ListView listView= (ListView)findViewById(R.id.listview_logs);
+        ListView listView = (ListView) findViewById(R.id.listview_logs);
 
         listView.setAdapter(mLogsAdapter);
 
@@ -42,12 +50,35 @@ public class Page2Activity extends ActionBarActivity {
             public void onClick(View view) {
                 EditText address = (EditText) findViewById(R.id.et_address);
                 String address_string = address.getText().toString();
-                address_string.replaceAll(" ","+");
-                address_string.replaceAll(",",",+");
+                address_string.replaceAll(" ", "+");
+                address_string.replaceAll(",", ",+");
                 HttpURLConnection urlConnection = null;
 
             }
         });
+
+        //VENUE BUTTON FUNCTIONALITY
+
+        Button venueButton = (Button) findViewById(R.id.venue_button);
+
+        venueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                venueClick(v);
+            }
+        });
+
+        venueButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                venueLongClick(v);
+                return false;
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -71,5 +102,55 @@ public class Page2Activity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Page2 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.google.devplat.lmoroney.maps3_1/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Page2 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.google.devplat.lmoroney.maps3_1/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    void venueClick(View view) {
+        //Relocate on Map
+    }
+
+    void venueLongClick(View view)
+    {
+        //Choose location from another activity
     }
 }
