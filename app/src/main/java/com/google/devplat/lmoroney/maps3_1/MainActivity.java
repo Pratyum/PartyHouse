8a9dd77fd08a866772526c072d734bd9010efae6
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-01-23 22:20:35 +0800

    kjdc,dsz

--- app/src/main/java/com/google/devplat/lmoroney/maps3_1/MainActivity.java
+++ app/src/main/java/com/google/devplat/lmoroney/maps3_1/MainActivity.java
@@ -18,26 +18,66 @@ package com.google.devplat.lmoroney.maps3_1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.parse.Parse;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
+    private ArrayAdapter<RelativeLayout> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this);
-        ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
-        startActivityForResult(builder.build(), 0);
 
+        setContentView(R.layout.activity_main);
+//        Parse.enableLocalDatastore(this);
+//        Parse.initialize(this);
+//        ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
+//        startActivityForResult(builder.build(), 0);
+
+        RelativeLayout r1 = createRelLay("Party1", "NUS\n9:31pm");
+        RelativeLayout r2 = createRelLay("Party2", "NUS\n9:32pm");
+        RelativeLayout r3 = createRelLay("Party3", "NUS\n9:33pm");
+        RelativeLayout r4 = createRelLay("Party4", "NUS\n9:34pm");
+        RelativeLayout r5 = createRelLay("Party5", "NUS\n9:35pm");
+        RelativeLayout r6 = createRelLay("Party6", "NUS\n9:36pm");
+
+        ArrayList<RelativeLayout> mLogs = new ArrayList<RelativeLayout>();
+        mLogs.add(r1);
+        mLogs.add(r2);
+        mLogs.add(r3);
+        mLogs.add(r4);
+        mLogs.add(r5);
+        mLogs.add(r6);
+
+
+        mAdapter = new ArrayAdapter<RelativeLayout>(this, R.layout.main_list, mLogs);
+
+        ListView listView = (ListView) findViewById(R.id.party_listview);
+
+        listView.setAdapter(mAdapter);
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            System.exit(0);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,14 +98,50 @@ public class MainActivity extends ActionBarActivity {
             Intent intent = new Intent(this, Page2Activity.class);
             startActivity(intent);
             return true;
-        }
-        else if (id == R.id.log_out){
+        } else if (id == R.id.log_out) {
             ParseUser.logOut();
             ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
             startActivityForResult(builder.build(), 0);
+            finish();
+            System.exit(0);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    RelativeLayout createRelLay(String one, String two)
+    {
+        // Definition
+        RelativeLayout l1 = new RelativeLayout(this);
+        TextView t1 = new TextView(this);
+        TextView t2 = new TextView(this);
+
+
+        //Adding content
+        t1.setText(one);
+        t2.setText(two);
+
+
+        //Creating LayoutParams
+        RelativeLayout.LayoutParams t1Details = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        RelativeLayout.LayoutParams t2Details = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        t1Details.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        t1Details.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        t2Details.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        t2Details.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        //Adding textviews to RelLay
+        l1.addView(t1,t1Details);
+        l1.addView(t2,t2Details);
+        return l1;
     }
 
 }
